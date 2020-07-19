8aff81e1ed3f0ce6df5a4b57c5a3eeb119120049
msurya007 <55659274+msurya007@users.noreply.github.com>
2020-07-19 23:38:18 +0530

    Update final.php

--- final.php
+++ final.php
@@ -1,18 +1,50 @@
+<!DOCTYPE HTML>
+<html lang = "en">
+<head>
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+  background-color: lightgrey;
+  opacity: 0.9;
+}
+body
+{
+  background-image: url("Images/images2.jpeg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+table{ padding-top: 10%;}
+th, td {
+  padding: 15px;
+}
+h1
+{
+  padding: 15px;
+}
+</style>
+     <title>Extract Names</title>
+</head>
+<body >
+     <h1><center>EXTRACTION</center></h1>
+</body>
+</html>
 <?php
-$tablet1 = $_POST['tablet1'];
-$tablet2 = $_POST['tablet2'];
+$name1 = $_POST['name1'];
+$name2 = $_POST['name2'];
 
  $con = new mysqli("localhost", "root", "", "test");
- if(!empty($tablet1) && !empty($tablet2)){
+ if(!empty($name1) && !empty($name2)){
 	 
-$result=mysqli_query($con,"SELECT * FROM `Medicine_Info` WHERE (`name` in ('$tablet1', '$tablet2'))");
+$result=mysqli_query($con,"SELECT * FROM `Medicine_Info` WHERE (`name` in ('$name1', '$name2'))");
 if(!empty($result)){
-	echo "<table>
+	echo "<center><table>
 			<tr>
 				<th>NAME</th>
 				<th>Protein name</th>
 				<th>PURPOSE</th>
-			</tr>";
+			</tr></center>";
 while($row = mysqli_fetch_array($result)) {
   echo "<tr>";
   echo "<td>" . $row['NAME'] . "</td>";
@@ -28,46 +60,3 @@ echo "</table>";
 	mysqli_close($con);
 
 ?>
-<!DOCTYPE HTML>
-<html lang = "en">
-<head>
-<style>
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-}
-th, td {
-  padding: 15px;
-}
-</style>
-<title>Example</title>
-</head>
-<body style="background-color:yellowgreen;color:white;">
-<h1><center>WELCOME</center></h1>
-</body>
-  <head>
-    <title>formDemo.html</title>
-    <meta charset = "UTF-8" />
-  </head>
-  <body>
-    <h1>Extraction</h1>
-    <form action="final.php" method="POST">
-      <fieldset>
-       
-        <p>
-                               <center><label>    <h1> Tablet 1</label>
-          <input type = "text" name = "tablet1"
-                placeholders = "text here"/>
-        </p>
-       
-         <p>
-          <label>Tablet 2</label>
-          <input type = "text"
-                 name="tablet2"
-                 placeholders = "text here" /></h1>
-        </p>
-        <input type="submit" value="Submit">
-      </fieldset>
-    </form>
-  </body>
-</html>
